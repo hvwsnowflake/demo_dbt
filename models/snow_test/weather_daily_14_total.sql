492d31f00a282985a316c52a71d1f2421781083e
hein.van.wijlen@vionfood.com <hein.van.wijlen@vionfood.com>
2023-01-24 16:10:11 +0100

    abac

--- models/snow_test/weather_daily_14_total.sql
+++ models/snow_test/weather_daily_14_total.sql
@@ -1,15 +1,23 @@
+{{ config(materialized="table") }}
+with
+    source_data as (
 
-with source_data as (
+        select
 
-    SELECT * FROM SNOWFLAKE_SAMPLE_DATA.weather.daily_14_total
+            v:city:coord:lat as latitude,
+            v:city:name as city,
 
-)
+            v:city:country::string as country,
+            t::date as date
+        from snowflake_sample_data.weather.daily_14_total
+        where t between '2020-01-01' and '2020-01-31'
+    )
+
+
 
 select *
 from source_data
-
 /*
     Uncomment the line below to remove records with null `id` values
 */
-
 -- where id is not null
